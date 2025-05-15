ee11b0bf7191d6171c5672ec3e5f75fa6837a0a8
njung <njung@aicom.co.jp>
2025-05-15 09:00:37 +0900

    initial commit

---
+++ tech-practice-project/src/main/java/org/noahjung/tech_practice_project/TechPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package org.noahjung.tech_practice_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechPracticeProjectApplication.class, args);
+	}
+
+}
---
+++ tech-practice-project/src/test/java/org/noahjung/tech_practice_project/TechPracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.noahjung.tech_practice_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechPracticeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
